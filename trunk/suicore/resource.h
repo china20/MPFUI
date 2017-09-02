5891bd1996a32d20e97be35b5ae171d75694eac8
china0851 <china0851@foxmail.com>
2017-09-02 18:21:59 +0800

    优化默认最大化按钮资源样式

--- trunk/suicore/resource.h
+++ trunk/suicore/resource.h
@@ -5,13 +5,13 @@
 #define IDC_RESIZE                      101
 #define IDC_SPLITH                      102
 #define IDC_SPLITV                      103
-#define DEFRES                          115
+#define DEFRES                          116
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        116
+#define _APS_NEXT_RESOURCE_VALUE        117
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
